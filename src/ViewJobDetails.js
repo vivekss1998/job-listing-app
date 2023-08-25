b164a9eb1ea48c0c0805ecc9d4366627ddb266f7
vivekss1998 <you@example.com>
2023-08-25 08:38:14 +0530

    social-media-icon-and-inner-html

--- src/ViewJobDetails.js
+++ src/ViewJobDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './ViewJobDetails.css'; // Import your CSS file for styling
+import './ViewJobDetails.css';
 
 const ViewJobDetails = () => {
   const { jobId } = useParams();
@@ -21,45 +21,66 @@ const ViewJobDetails = () => {
     return <p>Loading...</p>;
   }
 
+  const shareUrl = `https://demo.jobsoid.com/JobDetails/${jobId}`;
+  const shareText = `Check out this job: ${jobDetails.title} at ${jobDetails.company}`;
+
+
   return (
     <div className='container'>
       <div className='card'>
-    <div className="job-details">
-      {/* Title and Location Section */}
-      <div className="title-section">
-        <h2 className="job-title">{jobDetails.title}</h2>
-        <p className="location">
-          <i className="pi pi-map-marker location-icon" />
-          {jobDetails.location && jobDetails.location.city},{' '}
-          {jobDetails.location && jobDetails.location.state},{' '}
-          {jobDetails.location && jobDetails.location.country}
-        </p>
-      </div>
+        <div className="job-details">
+          {/* Title and Location Section */}
+          <div className="title-section">
+            <h2 className="job-title">{jobDetails.title}</h2>
+            <p className="location">
+              <i className="pi pi-map-marker location-icon" />
+              {jobDetails.location && jobDetails.location.city},{' '}
+              {jobDetails.location && jobDetails.location.state},{' '}
+              {jobDetails.location && jobDetails.location.country}
+            </p>
+          </div>
 
-      {/* Job Content Section */}
-      <div className="job-content">
-        <div className="info-section">
-          <strong className="posted-date">
-            Posted Date: {new Date(jobDetails.postedDate).toLocaleDateString()}
-          </strong>
-          <strong className="positions">Positions: {jobDetails.positions}</strong>
-          <strong className="location">
-            <i className="pi pi-map-marker location-icon" />
-            Location: {jobDetails.location && jobDetails.location.city},{' '}
-            {jobDetails.location && jobDetails.location.state},{' '}
-            {jobDetails.location && jobDetails.location.country}
-          </strong>
+          {/* Job Content Section */}
+          <div className="job-content">
+            <div className="info-section">
+              <strong className="posted-date">
+                Posted Date: {new Date(jobDetails.postedDate).toLocaleDateString()}
+              </strong>
+              <strong className="positions">Positions: {jobDetails.positions}</strong>
+              <strong className="location">
+                <i className="pi pi-map-marker location-icon" />
+                Location: {jobDetails.location && jobDetails.location.city},{' '}
+                {jobDetails.location && jobDetails.location.state},{' '}
+                {jobDetails.location && jobDetails.location.country}
+              </strong>
+            </div>
+
+            {/* Job Description */}
+            <div className="description-section">
+              <div dangerouslySetInnerHTML={{ __html: jobDetails.description }} />
+              <a href={jobDetails.applyUrl} target="_blank" rel="noopener noreferrer" className="apply-button">
+                Apply Now
+              </a>
+            </div>
+
+            {/* Social Media Share */}
+            <div className="share-section align justify-content-between">
+              <h3>Share this job:</h3>
+              <div className="social-icons">
+                <a href={`https://twitter.com/intent/tweet?url=${shareUrl}&text=${shareText}`} target="_blank" rel="noopener noreferrer">
+                  <i className="pi pi-twitter" />
+                </a>
+                <a href={`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`} target="_blank" rel="noopener noreferrer">
+                  <i className="pi pi-facebook" />
+                </a>
+                <a href={`https://www.linkedin.com/shareArticle?url=${shareUrl}&title=${shareText}`} target="_blank" rel="noopener noreferrer">
+                  <i className="pi pi-linkedin" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* Other job details */}
-        <a href={jobDetails.hostedUrl} target="_blank" rel="noopener noreferrer">
-          View Job Details
-        </a>
-        <a href={jobDetails.applyUrl} target="_blank" rel="noopener noreferrer">
-          Apply Now
-        </a>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
